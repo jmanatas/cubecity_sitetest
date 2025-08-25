c2cfee5f6c11d5806f5a1b372a18904ac640cfff
jmanatas <jmanatas@gmail.com>
2025-08-25 21:04:03 +0100

    new version with working avatar

--- src/avatar.js
+++ src/avatar.js
@@ -1,6 +1,6 @@
-import * as THREE from 'three';
-import { Capsule } from 'three/examples/jsm/math/Capsule.js';
-import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
+import * as THREE from 'https://cdn.jsdelivr.net/npm/three@0.162.0/build/three.module.js';
+import { Capsule } from 'https://cdn.jsdelivr.net/npm/three@0.162.0/examples/jsm/math/Capsule.js';
+import { GLTFLoader } from 'https://cdn.jsdelivr.net/npm/three@0.162.0/examples/jsm/loaders/GLTFLoader.js';
 import { AvatarController } from './avatarController.js';
 
 export class Avatar {
