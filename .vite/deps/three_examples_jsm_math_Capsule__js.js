df0763b6e4b7cf7b2d1f04b3a0e333b4adfec709
jmanatas <jmanatas@gmail.com>
2025-08-28 12:46:54 +0100

    Taking site online for the first time

--- .vite/deps/three_examples_jsm_math_Capsule__js.js
+++
@@ -1,7 +0,0 @@
-import {
-  Capsule
-} from "./chunk-QFYABUXB.js";
-import "./chunk-2HJJW66Q.js";
-export {
-  Capsule
-};
--- .vite/deps/three_examples_jsm_postprocessing_ShaderPass__js.js
+++
@@ -1,8 +0,0 @@
-import {
-  ShaderPass
-} from "./chunk-E3UZUUS4.js";
-import "./chunk-NMIQ3YBE.js";
-import "./chunk-2HJJW66Q.js";
-export {
-  ShaderPass
-};
