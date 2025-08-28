5f1a3855da64d4008c05c935f8d95d0f146d959d
jmanatas <jmanatas@gmail.com>
2025-08-28 16:34:53 +0100

    Taking site online for the first time

--- vite.config.js
+++ vite.config.js
@@ -2,13 +2,7 @@ import { defineConfig } from 'vite';
 import { fileURLToPath, URL } from 'node:url';
 
 export default defineConfig({
-  base: './',
-  server: {
-    port: 3000,
-    fs: {
-      strict: false // Allow serving files from outside root
-    }
-  },
+  base: process.env.NODE_ENV === 'production' ? '/cubecity_sitetest/' : '/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
