cc3c794fdb7422af56660344fb9701bbf740f6a1
jmanatas <jmanatas@gmail.com>
2025-09-09 17:00:57 +0100

    bunch of fixes, check the README

--- src/avatar.js
+++ src/avatar.js
@@ -1,4 +1,4 @@
-import * as THREE from 'three';
+import * as THREE from 'https://cdn.jsdelivr.net/npm/three@0.169.0/build/three.module.js';
 import { Capsule } from 'https://cdn.jsdelivr.net/npm/three@0.169.0/examples/jsm/math/Capsule.js';
 import { GLTFLoader } from 'https://cdn.jsdelivr.net/npm/three@0.169.0/examples/jsm/loaders/GLTFLoader.js';
 import { AvatarController } from './avatarController.js';
